78bd65b3bcd4be7021c0b8092b514c6d5d2989c1
linenmin <37617423+linenmin@users.noreply.github.com>
2025-03-11 20:59:22 +0100

    Update download_data.py
    change datasete link

--- download_data.py
+++ download_data.py
@@ -2,7 +2,7 @@ import os
 import gdown
 
 # Google Drive 共享文件的 ID（从你的链接提取）
-FILE_ID = "1o2eGgBUSksNGVoHxPuz-uZTEW3fZopvo"
+FILE_ID = "1DNbvVhwjcKDos7tOxXFgTwtD427VvyiC"
 OUTPUT_PATH = "data/timeStack_data_1281281.zip"
 
 # 确保 data 目录存在
